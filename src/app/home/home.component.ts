99098662e8a7b8e209216334d5d56393ec77b021
Jaya Bhattacharjee <jaya.bhattacharjee@happiestminds.com>
2020-04-11 16:52:31 +0530

    home button

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,137 +4,146 @@ import { saveAs } from 'file-saver';
 import { Router } from '@angular/router';
 import { UrlConfiguration, match, contest } from './../core/url-configuration';
 import { UtilityService } from './../core/utility.service';
-import {ɵBROWSER_SANITIZATION_PROVIDERS, DomSanitizer, ɵDomSanitizerImpl} from '@angular/platform-browser';
-
+import {
+  ɵBROWSER_SANITIZATION_PROVIDERS,
+  DomSanitizer,
+  ɵDomSanitizerImpl,
+} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
   matches: match[];
-  count : number;
-  img : any;
-  text:any = {
-    Days: "",
-    Hours: "",
-    Minutes: "",
-    Seconds: ""
+  count: number;
+  img: any;
+  text: any = {
+    Days: '',
+    Hours: '',
+    Minutes: '',
+    Seconds: '',
   };
 
-  constructor(private router: Router, private service: HomeService, private sanitizer: DomSanitizer, private util : UtilityService) { }
+  constructor(
+    private router: Router,
+    private service: HomeService,
+    private sanitizer: DomSanitizer,
+    private util: UtilityService
+  ) {}
 
   ngOnInit() {
-
     this.service.getUpcomingMatchData().subscribe(
-      data => {
+      (data) => {
         this.matches = data.data as match[];
         this.count = this.matches.length;
-        for(let m of this.matches){
+        for (const m of this.matches) {
           console.log(m.matchTime);
-          var time = m.matchTime.split("T");
-          var year = time[0].split("-");
-          var hour = time[1].split(":");
-          var gamedate = new Date(2020,3,15, 21, 0, 0, 0);
-          m.matchTimeinSec = gamedate.getMonth()+1 + ' '+ gamedate.getDate()+", "+gamedate.getFullYear()+", "+time[1];
-          console.log(m.matchTimeinSec);
+          const time = m.matchTime.split('T');
+          const year = time[0].split('-');
+          const hour = time[1].split(':');
+          const gamedate = new Date(2020, 3, 15, 21, 0, 0, 0);
+          m.matchTimeinSec =
+            gamedate.getMonth() +
+            1 +
+            ' ' +
+            gamedate.getDate() +
+            ', ' +
+            gamedate.getFullYear() +
+            ', ' +
+            time[1];
+          console.log(m);
           this.service.getContestData(m.id).subscribe(
-            data => {
+            (data) => {
               console.log(data);
-              let contests = data.data as contest[];
+              const contests = data.data as contest[];
               m.totalLeagues = contests.length;
-              let ammount =0;
-              for(let contest of contests){
+              let ammount = 0;
+              for (const contest of contests) {
                 ammount += contest.totalPrize;
               }
               m.totalMoney = ammount;
-              
-              console.log(m);
-              this.service.getTeamImage(m.category,m.team1Id).subscribe(
-                data => {
+              this.service.getTeamImage(m.category, m.team1Id).subscribe(
+                (data) => {
                   const blob = new Blob([data], { type: 'image/png' });
                   const url = window.URL.createObjectURL(blob);
-                  m.team1pic = this.sanitizer.bypassSecurityTrustUrl(url);             
+                  m.team1pic = this.sanitizer.bypassSecurityTrustUrl(url);
                 },
-                error => {
+                (error) => {
                   console.log(error);
-                });
+                }
+              );
 
-                this.service.getTeamImage(m.category,m.team2Id).subscribe(
-                  data => {
-                    const blob = new Blob([data], { type: 'image/png' });
-                    const url = window.URL.createObjectURL(blob);
-                    m.team2pic = this.sanitizer.bypassSecurityTrustUrl(url);
-                  },
-                  error => {
-                    console.log(error);
-                  });
+              this.service.getTeamImage(m.category, m.team2Id).subscribe(
+                (data) => {
+                  const blob = new Blob([data], { type: 'image/png' });
+                  const url = window.URL.createObjectURL(blob);
+                  m.team2pic = this.sanitizer.bypassSecurityTrustUrl(url);
+                },
+                (error) => {
+                  console.log(error);
+                }
+              );
             },
-            error => {
-            console.log(error);
+            (error) => {
+              console.log(error);
             }
           );
-          }
-          
-        },
-      error => {
-        console.log(error)
-        if(error.status===401)
-        {
-          localStorage.removeItem("token");
-          localStorage.removeItem("userName");
-          this.router.navigateByUrl("/login");
         }
-        
-      });
+      },
+      (error) => {
+        console.log(error);
+        if (error.status === 401) {
+          localStorage.removeItem('token');
+          localStorage.removeItem('userName');
+          this.router.navigateByUrl('/login');
+        }
+      }
+    );
   }
 
-  saveData(match: match, contests: contest[]){
+  saveData(match: match, contests: contest[]) {
     this.util.contests = contests;
     this.util.matches = this.matches;
     this.util.currentMatch = match;
   }
 
-  onShowTeamsClick(matchId: any){
-    for(let match of this.matches){
-      if(match.id===matchId)
-      {
+  onShowTeamsClick(matchId: any) {
+    for (const match of this.matches) {
+      if (match.id === matchId) {
         this.service.getContestData(match.id).subscribe(
-          data => {
+          (data) => {
             console.log(data);
-            let contests = data.data as contest[];
-            this.saveData(match,contests);
-            this.router.navigateByUrl("/showTeam");
+            const contests = data.data as contest[];
+            this.saveData(match, contests);
+            this.router.navigateByUrl('/showTeam');
           },
-          error => {
-          console.log(error);
+          (error) => {
+            console.log(error);
           }
         );
       }
     }
   }
 
-  onCreateTeamClick(matchId: any){
-
+  onCreateTeamClick(matchId: any) {
     this.util.editTeam = false;
-    for(let match of this.matches){
-      if(match.id===matchId)
-      {
+    for (const match of this.matches) {
+      if (match.id === matchId) {
         this.service.getContestData(match.id).subscribe(
-          data => {
+          (data) => {
             console.log(data);
-            let contests = data.data as contest[];
-            this.saveData(match,contests);
+            const contests = data.data as contest[];
+            this.saveData(match, contests);
             this.util.editTeam = false;
-            this.router.navigateByUrl("/createTeam");
+            this.router.navigateByUrl('/createTeam');
           },
-          error => {
-          console.log(error);
+          (error) => {
+            console.log(error);
           }
         );
       }
     }
-    
   }
 }
